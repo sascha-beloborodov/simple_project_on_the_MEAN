feb364cb5b8156716a399bf12337b4e65b70cc22
Sascha Beloborodov <a.beloborodov@hotmail.com>
2016-03-29 19:38:29 +0300

    created other api methods

--- models/Posts.js
+++ models/Posts.js
@@ -1,20 +1,23 @@
 // TODO: need to create simple orm
 
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'mean'
-});
+var connection;
+if (!connection) {
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'mean'
+  }); 
+}
 
 module.exports = {
   query: function (sqlQuery, callback) {
-    connection.connect();
+    // connection.connect();
     connection.query(sqlQuery, function(err, rows, fields) {
       if (err) throw err;
       callback(rows);
     });
-    connection.end();
+    // connection.end();
   }
 };
--- routes/posts.js
+++ routes/posts.js
@@ -13,18 +13,139 @@ router.get('/', function(req, response, next) {
   });
 });
 
+router.get('/:id', function(req, response, next) {
+  var id = Number(req.params.id);
+  if (!Number.isInteger(Number(id))) {
+    throw "Invalid parameter";
+  }
+  var sql = "SELECT * FROM posts WHERE id=" + id;
+  
+  // var res;
+  Post.query(sql, function(result) {
+    res = result;
+    response.json(res[0]);
+  });
+});
+
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
-  var sql = "INSERT INTO posts (`title`, `link`) VALUES ('" + req.body.title + "', '"+req.body.link+ "');";
-  Post.query(sql, function(result) {
-    console.log(result);
-  });
-  // post.save(function(err, post){
-  //   if(err){ return next(err); }
+  var sql = "INSERT INTO posts (`title`, `link`) VALUES ('" + req.body.title + "', '" + req.body.link + "');", 
+    idNewRecord, post;
 
-  //   res.json(post);
-  // });
+  
+  var promise = new Promise(function(resolve, reject) {
+    try {
+      Post.query(sql, function(result) {
+        // nothing
+        idNewRecord = result.insertId;
+        resolve();
+      });
+    }
+    catch(e) {
+      console.log("Can not get data");
+      reject();
+    }
+  });
+  promise.then(function (val) {
+    Post.query("SELECT * FROM posts WHERE id=" + idNewRecord, function(result) {
+      res.json(post[0]);
+    });
+  });
+  
+  
+});
+
+//TODO: this method must be PUT
+router.post('/:id/upvote', function(req, res, next) {
+  var id = Number(req.params.id);
+  if (!Number.isInteger(Number(id))) throw "Invalid parameter";
+  
+  var post;
+  var upvotes;
+  var promise = new Promise(function(resolve, reject) {
+    try {
+      Post.query("SELECT * FROM posts WHERE id="+id, function(result) {
+        post = result[0];
+        upvotes = Number(post.upvotes) + 1;
+        post.upvotes = upvotes;
+        resolve();
+      });
+    }
+    catch(e) {
+      console.log("Can not get data");
+      reject();
+    }
+  });
+  promise.then(function (val) {
+    Post.query("UPDATE posts SET upvotes =" + upvotes + " WHERE id =" +id, function(result) {
+      // nothing
+    });
+    res.json(post);
+  });
+});
+
+
+
+router.post('/:id/comments', function(req, res, next) {
+  var id = Number(req.params.id), idNewRecord;
+  if (!Number.isInteger(Number(id))) throw "Invalid parameter";
+  
+  var sql = "INSERT INTO comments (post_id, text, author) VALUES(" + 
+    id + ", '" + req.body.text + "', '" + req.body.author + "')";
+
+  var promise = new Promise(function(resolve, reject) {
+    try {
+      Post.query(sql, function(result) {
+        idNewRecord = result.insertId;
+        resolve();
+      });
+    }
+    catch(e) {
+      console.log("Can not get data");
+      reject();
+    }
+  });
+  promise.then(function (val) {
+
+    res.json({
+      author: req.body.author,
+      text: req.body.text,
+      id: idNewRecord
+    });
+  });
+});
+
+
+
+router.post('/:id/comments/:cId/upvote', function(req, res, next) {
+  var id = Number(req.params.id);
+  var cId = Number(req.params.cId);
+  if (!Number.isInteger(Number(id)) || 
+    !Number.isInteger(Number(cId))) throw "Invalid parameter";
+  
+  var comment;
+  var upvotes;
+  var promise = new Promise(function(resolve, reject) {
+    try {
+      Post.query("SELECT * FROM commets WHERE id="+cId, function(result) {
+        comment = result[0];
+        upvotes = Number(comment.upvotes) + 1;
+        comment.upvotes = upvotes;
+        resolve();
+      });
+    }
+    catch(e) {
+      console.log("Can not get data");
+      reject();
+    }
+  });
+  promise.then(function (val) {
+    Post.query("UPDATE commets SET upvotes =" + upvotes + " WHERE id =" +cId, function(result) {
+      // nothing
+    });
+    res.json(comment);
+  });
 });
 
 module.exports = router;
